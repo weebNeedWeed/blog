dcc2e8022234a6d8bc37c2c7224dd1b4ec1e927f
weebNeedWeed <rivenmle@gmail.com>
2025-04-18 20:39:18 +0700

    feat(posts): add new blog post on Vibe Coding with complete content and table of contents

--- src/data/posts.js
+++ src/data/posts.js
@@ -1,12 +1,12 @@
 const posts = [
   {
-    title: "This is how I do Vide Coding my blog In 30 minutes!",
-    slug: "how-i-do-vide-coding-my-blog",
+    title: "This is how I do Vibe Coding my Blog In 30 minutes!",
+    slug: "how-i-do-vibe-coding-my-blog",
     date: "April 16, 2025",
     excerpt:
       "Welcome to my personal blog! Here I'll share my thoughts and experiences.",
     readingTime: "4 min read",
-    contentFilePath: "how-i-do-vide-coding-my-blog.html",
+    contentFilePath: "how-i-do-vibe-coding-my-blog.html",
   },
   {
     title: "Hi! I'm Harley. Welcome to my Blog",
